ea5bcb06c1d485bb4914ddfd944c82b01438a54e
SanjayKannan24 <sanjaykannan389@gmail.com>
2022-04-03 00:18:19 -0400

    lab test 2 mobile application

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Storage } from '@capacitor/storage';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,44 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  lat;
+  lng;
+
+  locationList = [] // Array to store list elements
+  constructor(private geo: Geolocation   ) {
+    
+  }
+
+  removeLocation(index){
+    this.locationList.splice(index,1)
+    const removeItem = async()=>{
+      await Storage.remove({key: index})
+    }
+    removeItem()
+    
+  }
+  
+  setLocation = async ()=>{
+    const location = JSON.stringify([{
+      timestamp: Date(),
+      lati: this.lat,
+      longi:this.lng
+    }])
+
+    await Storage.set({key:Date(), value: location})
+    console.log(Date())
+    console.log(await Storage.get({key:Date()}))
+  }
+  myLocation(){
+    this.geo.getCurrentPosition().then((res)=>{
+      this.lat = res.coords.latitude;
+      this.lng = res.coords.longitude;
+      this.locationList.push(JSON.stringify([this.lat, this.lng]))
+      console.log(this.locationList)
+      this.setLocation();
+    }).catch((error)=>{
+      console.log('Error getting the Location', error)
+    })
+  }
 
 }
